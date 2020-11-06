b1924734ec9285a02e1abbba8ae22d5155017165
Adrian Kubik <aver4@o2.pl>
2020-11-06 21:34:27 +0100

    Basic store config

--- src/components/Menu/MenuComponent.js
+++ src/components/Menu/MenuComponent.js
@@ -1,25 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import { Menu, Segment } from 'semantic-ui-react';
 
-const MenuComponent = () => (
+const MenuComponent = (props) => (
     <Segment inverted basic>
         <Menu inverted secondary pointing>
-            <Menu.Item name='Convert'
-                onClick={() => console.log('Convert')}></Menu.Item>
-            <Menu.Item name='Images'
-                onClick={() => console.log('Images')}></Menu.Item>
+            {props.isLoggedIn ? <React.Fragment>
+                <Menu.Item name='Convert'
+                    onClick={() => console.log('Convert')}></Menu.Item>
+                <Menu.Item name='Images'
+                    onClick={() => console.log('Images')}></Menu.Item>
+            </React.Fragment> : null}
             <Menu.Menu position="right">
-                <Menu.Item name='Login'
-                    onClick={() => console.log('Login')}></Menu.Item>
-                <Menu.Item name='Rgister'
-                    onClick={() => console.log('Register')}></Menu.Item>
-                <Menu.Item name='Logout'
-                    onClick={() => console.log('Logout')}></Menu.Item>
+                {!props.isLoggedIn ? <React.Fragment>
+                    <Menu.Item name='Login'
+                        onClick={() => console.log('Login')}></Menu.Item>
+                    <Menu.Item name='Register'
+                        onClick={() => console.log('Register')}></Menu.Item>
+                </React.Fragment> : null}
+                {props.isLoggedIn ? <Menu.Item name='Logout'
+                    onClick={() => console.log('Logout')}></Menu.Item> : null}
             </Menu.Menu>
         </Menu>
 
     </Segment>
-)
+);
 
-export default MenuComponent;
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.isLoggedIn
+    }
+}
+
+export default connect(mapStateToProps)(MenuComponent);
